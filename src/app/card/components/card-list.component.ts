9189aa7e11a50e0ba87459d95866c8845fa0f0e1
Ella Cornell <ellacornell.nordcode@gmail.com>
2019-01-22 20:22:54 +0700

    successfully showing all card decks using card list component part 2

--- src/app/card/components/card-list.component.ts
+++ src/app/card/components/card-list.component.ts
@@ -1,8 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'app-card-list',
     templateUrl: './card-list.component.html'
 })
 
-export class CardListComponent {}
+export class CardListComponent {
+    @Input() items: any[] = [];
+    @Input() listName: string;
+}
