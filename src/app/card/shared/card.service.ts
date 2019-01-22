6806c2b648a16e10bf064744316b1a7fc1069bbb
Ella Cornell <ellacornell.nordcode@gmail.com>
2019-01-22 18:51:11 +0700

    Christy fixes API data, not completed

--- src/app/card/shared/card.service.ts
+++ src/app/card/shared/card.service.ts
@@ -6,23 +6,23 @@ import {CardDeck} from './card.model';
 @Injectable()
 export class CardService {
 
-    // private readonly HS_API_URL = 'https://omgvamp-hearthstone-v1.p.mashape.com';
-    // private readonly API_KEY = 'TloU2Nmkphmsh7zNOZqu3tewm8fcp1kJqOtjsnYiQHoRo6wO4g';
+    private readonly HS_API_URL = 'https://omgvamp-hearthstone-v1.p.mashape.com';
+    private readonly API_KEY = 'TloU2Nmkphmsh7zNOZqu3tewm8fcp1kJqOtjsnYiQHoRo6wO4g';
 
-    private readonly cardDecks: string[] = [
-        'Druid', 'Mage', 'Warrior', 'Rogue', 'Shaman', 'Priest',
-        'Warlock', 'Hunter', 'Paladin'
-    ];
-    //
-    // constructor(private http: HttpClient) {
-    // }
+    // private readonly cardDecks: string[] = [
+    //     'Druid', 'Mage', 'Warrior', 'Rogue', 'Shaman', 'Priest',
+    //     'Warlock', 'Hunter', 'Paladin'
+    // ];
 
-    // public getAllCardDecks(): Observable<CardDeck[]> {
-    //     const headers = new HttpHeaders({'X-Mashape-Key': this.API_KEY});
-    //     return this.http.get<CardDeck[]>(`${this.HS_API_URL}/info`, {headers});
-    // }
+    constructor(private http: HttpClient) {
+    }
 
     public getAllCardDecks(): Observable<CardDeck[]> {
-        return ObservableOf(this.cardDecks);
+        const headers = new HttpHeaders({'X-Mashape-Key': this.API_KEY});
+        return this.http.get<CardDeck[]>(`${this.HS_API_URL}/info`, {headers});
     }
+
+    // public getAllCardDecks(): Observable<CardDeck[]> {
+    //     return ObservableOf(this.cardDecks);
+    // }
 }
