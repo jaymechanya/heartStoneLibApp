dd8607420585805485e8027e994dff675e380f35
Jayme Chanya <jaymec.dev@gmail.com>
2019-01-25 22:13:40 +0700

    Add Loader

--- src/app/card/card-detail/card-detail.page.ts
+++ src/app/card/card-detail/card-detail.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {CardService} from '../shared/card.service';
 import {Card} from '../shared/card.model';
+import {LoadingController} from '@ionic/angular';
 
 @Component({
   selector: 'app-card-detail',
@@ -11,20 +12,34 @@ import {Card} from '../shared/card.model';
 export class CardDetailPage {
 
   card: Card;
+  loader: any;
 
   constructor(private route: ActivatedRoute,
-              private cardService: CardService) {}
+              private cardService: CardService,
+              private loadingCtrl: LoadingController) {}
 
-  ionViewWillEnter() {
+  private async presentLoading() {
+    const loader = await this.loadingCtrl.create({
+      content: 'Loading..',
+      translucent: true
+    });
+    loader.present();
+    return loader;
+  }
+
+  async ionViewWillEnter() {
     const cardId = this.route.snapshot.paramMap.get('cardId');
+
+    this.loader = await this.presentLoading();
 
     this.cardService.getCardbyId(cardId).subscribe(
         (card: Card[]) => {
           this.card = card.map((card: Card) => {
-            // card.text = card.text ? card.text.replace(new RegExp("\\\\n", "g"), ", ") : 'No Description';
             card.text = this.cardService.replaceCardTextLine(card.text);
+            // card.text = card.text ? card.text.replace(new RegExp("\\\\n", "g"), ", ") : 'No Description';
             return card;
           })[0];
+          this.loader.dismiss();
         });
   }
 
--- src/app/card/card-listing/card-listing.page.ts
+++ src/app/card/card-listing/card-listing.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {CardService} from '../shared/card.service';
 import {Card} from '../shared/card.model';
+import {LoadingController} from '@ionic/angular';
+import {tsStructureIsReused} from '@angular/compiler-cli/src/transformers/util';
 
 @Component({
   selector: 'app-card-listing',
@@ -12,24 +14,37 @@ export class CardListingPage {
 
   cardDeckGroup: string;
   cardDeck: string;
-
   cards: Card[] = [];
+  loader: any;
 
   constructor(private route: ActivatedRoute,
-              private cardService: CardService) {}
+              private cardService: CardService,
+              private loadingCtrl: LoadingController) {}
 
-  ionViewWillEnter() {
+  private async presentLoading() {
+    const loader = await this.loadingCtrl.create({
+      content: 'Loading..',
+      translucent: true
+    });
+    loader.present();
+    return loader;
+  }
+
+  async ionViewWillEnter() {
     this.cardDeckGroup = this.route.snapshot.paramMap.get('cardDeckGroup');
     this.cardDeck = this.route.snapshot.paramMap.get('cardDeck');
 
+    this.loader = await this.presentLoading();
+
     this.cardService.getCardsByDeck(this.cardDeckGroup, this.cardDeck).subscribe(
-        (cards: Card[]) => {
-          this.cards = cards.map((card: Card) => {
-            // card.text = card.text ? card.text.replace(new RegExp("\\\\n", "g"), ", ") : 'No Description';
+    (cards: Card[]) => {
+      this.cards = cards.map((card: Card) => {
             card.text = this.cardService.replaceCardTextLine(card.text);
+            // card.text = card.text ? card.text.replace(new RegExp("\\\\n", "g"), ", ") : 'No Description';
             return card;
-          });
-        });
+      });
+      this.loader.dismiss();
+    });
   }
 
 }
